95dfd1ac257bcf591b8ef74b42378c221203cbb6
Zachary Karpinski <zkarpinski1@gmail.com>
2014-07-21 12:45:21 -0400

    added docx conversion parameters and save folder.

--- SAMuel/Modules/ConvertToTiff.vb
+++ SAMuel/Modules/ConvertToTiff.vb
@@ -7,7 +7,7 @@ Module ConvertToTiff
         'Converts Word Documents to .tif using MODI
         Dim objWdDoc As Word.Document
         Dim objWord As Word.Application
-        Dim sDestination As String = My.Settings.savePath
+        Dim sDestination As String = My.Settings.savePath + "converted\"
         Dim sFileName As String
 
         'Initiate word application object and minimize it
@@ -16,11 +16,14 @@ Module ConvertToTiff
         'Set active printer to Fax
         objWord.ActivePrinter = "Microsoft Office Document Image Writer"
 
+        'Verify output folder exists
+        GlobalModule.CheckFolder(sDestination)
+
         For Each value In sFiles
             'Get the file name
             sFileName = Path.GetFileNameWithoutExtension(value)
-            'Open the document within word
-            objWdDoc = objWord.Documents.Open(value)
+            'Open the document within word and don't prompt  for conversion.
+            objWdDoc = objWord.Documents.Open(FileName:=value, ConfirmConversions:=False, AddToRecentFiles:=False, [ReadOnly]:=True, Visible:=False)
             objWord.Visible = False
             'Print to Tiff
             objWdDoc.PrintOut(PrintToFile:=True, OutputFileName:=sDestination & sFileName & ".tif")
